b0994733210a7fcd3612067b01235c7867524f5a
Buket Özceylan <buketozceylan@outlook.com>
2025-12-05 17:52:34 +0300

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ st.title('Am I going to be rich? Or Am I already rich??')
 
 st.write("This app is for funny purposes only. This has no meaning and won't have any meaning in the future. ")
 
-st.dataframe(df)
+st.dataframe(class_model.df)
