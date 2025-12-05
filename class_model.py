242aa901170ad4fcbf58b789ff75312493d1c580
Buket Özceylan <buketozceylan@outlook.com>
2025-12-05 17:25:12 +0300

    test

---
+++ class_model.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error,classification_report, confusion_matrix,accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.compose import ColumnTransformer
+
+df = pd.read_csv("main-data.csv")
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import class_model
 
 st.title('Am I going to be rich? Or Am I already rich??')
 
 st.write("This app is for funny purposes only. This has no meaning and won't have any meaning in the future. ")
+
+df
