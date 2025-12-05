2d32f7d2cc2069d0ea52a36dce6dd61007394b87
Buket Özceylan <buketozceylan@outlook.com>
2025-12-05 17:50:07 +0300

    test

--- model/class_model.py
+++ model/class_model.py
@@ -1,8 +1,6 @@
 import matplotlib
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-%matplotlib inline
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
