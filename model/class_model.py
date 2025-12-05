0406a9ee57c38a45d295ac9acd1c7aa060b5740b
Buket Özceylan <buketozceylan@outlook.com>
2025-12-05 17:59:01 +0300

    test

--- model/class_model.py
+++ model/class_model.py
@@ -1,3 +1,42 @@
 import pandas as pd
+from sklearn.preprocessing import RobustScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv("main-data.csv")
+
+cat_col = ["Experience Level", "Employment Type","Job Title"]
+
+ordinal_encoder = OrdinalEncoder(categories = [["SE","MI","EN","EX"],["FT","PT","CT","FL"],["Data Scientist","Data Engineer","Data Analyst","Machine Learning Engineer"]])
+
+preprocessor = ColumnTransformer(transformers = [
+    ("transformation_name",ordinal_encoder,cat_col)
+],remainder = "passthrough")
+
+
+remote = [0,1,2]
+for i in df["Remote?"].unique():
+    if i == 0:
+        df["Remote?"] = df["Remote?"].replace(i, 0)
+    elif i == 50:
+        df["Remote?"] = df["Remote?"].replace(i, 1)
+    elif i == 100:
+        df["Remote?"] = df["Remote?"].replace(i, 2)
+        
+
+X = df.drop("Is rich?", axis = 1)
+y = df["Is rich?"]
+
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.20, random_state=15)
+X_cols = X_train.columns
+X_train_transformed = preprocessor.fit_transform(X_train)
+X_test_transformed = preprocessor.transform(X_test)
+
+scaler = RobustScaler()
+cols = preprocessor.get_feature_names_out()
+X_train = pd.DataFrame(X_train_transformed, columns = cols, index = X_train.index)
+X_test = pd.DataFrame(X_test_transformed, columns = cols, index = X_test.index)
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+X_train = pd.DataFrame(X_train, columns = X_cols)
+X_test = pd.DataFrame(X_test, columns = X_cols)
--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,5 @@ st.title('Am I going to be rich? Or Am I already rich??')
 
 st.write("This app is for funny purposes only. This has no meaning and won't have any meaning in the future. ")
 
-st.dataframe(class_model.df)
+st.dataframe(class_model.X_train)
+
